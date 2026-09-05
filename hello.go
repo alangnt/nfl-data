50eb64b6c72e9e66e6dc66188777715e479ef46c
Alan Geirnaert <geirnaert.alan@icloud.com>
2026-09-05 10:45:36 +0200

    feat: main file init + logic added

---
+++ hello.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"nfl-data/utils"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	conference := utils.GetConference()
+
+	var teamId string
+
+	if conference == "afc" {
+		teamId = utils.GetAFCTeamId()
+	} else {
+		teamId = utils.GetNFCTeamId()
+	}
+
+	choice := utils.GetTeamInfoChoice()
+	if choice == "roster" {
+		utils.GetTeam(&teamId)
+	} else {
+		year := utils.GetYear()
+		utils.GetSchedule(&year, &teamId)
+	}
+}
